8ea74f4a3a80c6075855f66880cb51f2c6c6ab2e
TealMunkhtavilan <teal.2022.tavilan.erdenetovch@gmail.com>
2022-12-02 19:35:15 +0800

    JS progress 65%

--- Assignments/JS-Basic/js/main.js
+++ Assignments/JS-Basic/js/main.js
@@ -50,14 +50,88 @@
 // let convert = Math.pow(1024, 3) * bit * gb;
 // console.log("GB to bit: " + convert);
 
-//14 15MB data 3 second -d tatdag bol BIT Rate -iig olno.
+// //14 15MB data 3 second -d tatdag bol BIT Rate -iig olno.
+// let mb = 15;
+// let sec = 3;
+// let bitRate = (mb * 1000000 * 8) / 3;
+// console.log("Tanii bitrate: " + bitRate + "bit/s baina.");
+
+// //15 side1, side2, side3 -d utga onooj, gurvaljnii talbaig olno.
+// let side1 = 9, side2 = 11, side3 = 10;
+// let S = (side1 + side2 + side3) / 2;
+// let A = Math.sqrt(S * (S - side1) * (S - side2) * (S - side3));
+// console.log("Gurvaljnii talbai: " + A);
 
 
-//15 side1, side2, side3 -d utga onooj, gurvaljnii talbaig olnno.
+// //16 C to F
+// let C = parseInt(prompt("Celsus: ", ""));
+// let F = (C * 1.8) + 32;
+// console.log(F);
 
-//16 C to F
+// //17 F to C
+// let F = parseInt(prompt("Farenheit: ", ""));
+// let C = (F - 32) / 1.8;
+// console.log(C);
 
-//17 F to C
+// //18
+// let a = 15;
+// let b = 10;
+// let niilber, yalgavar, noogdvor, urjver, uldegdel;
+// niilber = a + b;
+// yalgavar = a - b;
+// noogdvor = a / b;
+// urjver = a * b;
+// uldegdel = a % b;
+// console.log("Ugugdsun 2 toonii niilber: " + niilber);
+// console.log("Ugugdsun 2 toonii yalgavar: " + yalgavar);
+// console.log("Ugugdun 2 toonii urjver: " + urjver);
+// console.log("Ugugdsun 2 toonii noogdvor: " + noogdvor);
+// console.log("Ugugdsun 2 toonii uldegdel: " + uldegdel);
+
+// //19
+// let cm = 5;
+// let inch = cm * 0.39;
+// console.log("cm to inch: " + inch);
+// //19b
+// let inchB = 1.95;
+// let cmB = inchB / 0.39;
+// console.log("inch to cm: " + cm);
+
+// //20
+// let r = parseInt(prompt("Radiusiig oruulna uu: ", ""));
+// let P = 2 * Math.PI * r;
+// let A = 2 * Math.PI * Math.pow(r, 2);
+// console.log("Duguin perimetr: " + P);
+// console.log("Duguin talbai: " + A);
+
+// //21
+// let number = 123;
+// let getFirstDigit = parseInt((number / 100)); // int turul ruu horvuulne;
+// console.log("Ehnii tsifr: " + getFirstDigit);
+
+//22
+// let babyShark = "Baby Shark, doo-doo, doo-doo\nBaby Shark, doo-doo, doo-doo\nBaby Shark, doo-doo, doo-doo\nBaby Shark, doo-doo, doo-doo\nBaby Shark";
+// let mommyShark = "Mommy Shark, doo-doo, doo-doo\nMommy Shark, doo-doo, doo-doo\nMommy Shark, doo-doo, doo-doo\nMommy Shark";
+// let daddyShark = "Daddy Shark, doo-doo, doo-doo\nDaddy Shark, doo-doo, doo-doo\nDaddy Shark, doo-doo, doo-doo\nDaddy Shark";
+// let grandmaShark = "Grandma Shark, doo-doo, doo-doo\nGrandma Shark, doo-doo, doo-doo\nGrandma Shark, doo-doo, doo-doo\nGrandma Shark";
+// let grandpaShark = "Grandpa Shark, doo-doo, doo-doo\nGrandpa Shark, doo-doo, doo-doo\nGrandpa Shark, doo-doo, doo-doo\nGrandpa Shark";
+// let hunt = "Let's go hunt, doo-doo, doo-doo\nLet's go hunt, doo-doo, doo-doo\nLet's go hunt, doo-doo, doo-doo\nLet's go hunt";
+// let run = "Run away, doo-doo, doo-doo\nRun away, doo-doo, doo-doo\nRun away, doo-doo, doo-doo\nRun away (ah!)";
+// let safe = "Safe at last, doo-doo, doo-doo\nSafe at last, doo-doo, doo-doo\nSafe at last, doo-doo, doo-doo\nSafe at last (phew)";
+// let end = "It's the end, doo-doo, doo-doo\nIt's the end, doo-doo, doo-doo\nIt's the end, doo-doo, doo-doo\nIt's the end";
+// console.log(babyShark);
+// console.log(mommyShark);
+// console.log(daddyShark);
+// console.log(grandmaShark);
+// console.log(grandpaShark);
+// console.log(hunt);
+// console.log(run);
+// console.log(safe);
+// console.log(end);
+
+
+
+
 
 
 
