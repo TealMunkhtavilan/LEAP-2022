499d94875c67b8b3e8deac8a9eef44c110892267
TealMunkhtavilan <teal.2022.tavilan.erdenetovch@gmail.com>
2022-12-13 18:45:05 +0800

    JS-DOM stopwatch (-rep)

--- Assignments/JS-DOM/Stopwatch/js/main.js
+++ Assignments/JS-DOM/Stopwatch/js/main.js
@@ -1,1 +1,38 @@
-document.getElementById("minutes").innerHTML = "50" + Date();
+let tens = 0, seconds = 0, minutes = 0;
+
+let getEl = document.querySelector('h1');
+setInterval(stopWatch, 100);
+function stopWatch()
+{
+    let stopwatch = '';
+    if(tens < 10)
+    {
+        stopwatch += "0"+ tens;
+        tens++;
+    }
+    if(tens > 10)
+    {
+        stopwatch = seconds + 1;
+        stopwatch += ":";
+        tens = 0;
+    }
+    stopwatch += tens;
+    if(seconds < 10)
+    {
+        stopwatch += "0"+ seconds;
+        
+    }
+    else if(seconds < 60)
+    {
+        stopwatch = seconds + 1;
+        stopwatch += ":";
+    }
+    else
+    {
+        stopwatch = ":" + minutes + 1;
+        seconds = 0;
+    }
+    stopwatch += minutes;
+    getEl.innerHTML = stopwatch;
+}
+
