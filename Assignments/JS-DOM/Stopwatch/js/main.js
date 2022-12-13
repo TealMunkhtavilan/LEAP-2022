fd20ecb10f63f1999ecf1ee5d8788362ff339cc0
Munkhtavilan <teal.2022.tavilan.erdenetovch@gmail.com>
2022-12-13 23:02:42 +0800

    JS-DOM Stopwatch (rep+)

--- Assignments/JS-DOM/Stopwatch/js/main.js
+++ Assignments/JS-DOM/Stopwatch/js/main.js
@@ -1,38 +1,51 @@
-let tens = 0, seconds = 0, minutes = 0;
-
-let getEl = document.querySelector('h1');
-setInterval(stopWatch, 100);
+let count = 1;
+let second = 1;
+let minute = 1;
 function stopWatch()
 {
-    let stopwatch = '';
-    if(tens < 10)
+    if(count < 10)
     {
-        stopwatch += "0"+ tens;
-        tens++;
+        document.getElementById("millsecond").innerHTML = count = "0" + count;
     }
-    if(tens > 10)
+    else if (count == 10)
     {
-        stopwatch = seconds + 1;
-        stopwatch += ":";
-        tens = 0;
+        count = 0;
+        document.getElementById("millsecond").innerHTML = count = "0" + count;
+        if(second < 10)
+        {
+            document.getElementById("second").innerHTML = second = "0" + second;
+        }
+        else if (second < 60)
+        {
+            document.getElementById("second").innerHTML = second;
+        }
+        else if (second == 60)
+        {
+            second = 0;
+            document.getElementById("second").innerHTML = "0" + second;
+            if(minute < 10)
+            {
+                document.getElementById("minute").innerHTML = "0" + minute;
+            }
+            else if(minute < 60)
+            {
+                document.getElementById("minute").innerHTML = minute;
+            }
+            else if (minute == 60)
+            {
+                count = 0;
+                second = 0;
+                minute = 0;
+            }
+            minute++;
+        }
+        second++;
     }
-    stopwatch += tens;
-    if(seconds < 10)
-    {
-        stopwatch += "0"+ seconds;
-        
-    }
-    else if(seconds < 60)
-    {
-        stopwatch = seconds + 1;
-        stopwatch += ":";
-    }
-    else
-    {
-        stopwatch = ":" + minutes + 1;
-        seconds = 0;
-    }
-    stopwatch += minutes;
-    getEl.innerHTML = stopwatch;
+    count++;
+    console.log(count);
 }
+
+setInterval(stopWatch, 100);
+
+
 
